d1cddf8bad19fb6023ad106cb777af855d7db668
Con Kolivas <kernel@kolivas.org>
2012-02-21 22:23:07 +1100

    Update licensing to GPL V3.

--- device-cpu.c
+++ device-cpu.c
@@ -1,4 +1,3 @@
-
 /*
  * Copyright 2011-2012 Con Kolivas
  * Copyright 2011-2012 Luke Dashjr
@@ -6,7 +5,7 @@
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
+ * Software Foundation; either version 3 of the License, or (at your option)
  * any later version.  See COPYING for more details.
  */
 
