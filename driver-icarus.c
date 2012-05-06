e195a31c0267a399a87bc3e536d4d12c8bdc8d02
Con Kolivas <kernel@kolivas.org>
2012-05-06 20:49:06 +1000

    Fix win32 build.

--- driver-icarus.c
+++ driver-icarus.c
@@ -46,18 +46,6 @@
 #else
   #include <windows.h>
   #include <io.h>
-
-  #ifndef timersub
-    #define timersub(a, b, result)                     \
-    do {                                               \
-      (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;    \
-      (result)->tv_usec = (a)->tv_usec - (b)->tv_usec; \
-      if ((result)->tv_usec < 0) {                     \
-        --(result)->tv_sec;                            \
-        (result)->tv_usec += 1000000;                  \
-      }                                                \
-    } while (0)
-  #endif
 #endif
 
 #include "elist.h"
--- miner.h
+++ miner.h
@@ -57,6 +57,33 @@ void *alloca (size_t);
  #endif
 #endif
 
+#ifdef WIN32
+  #ifndef timersub
+    #define timersub(a, b, result)                     \
+    do {                                               \
+      (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;    \
+      (result)->tv_usec = (a)->tv_usec - (b)->tv_usec; \
+      if ((result)->tv_usec < 0) {                     \
+        --(result)->tv_sec;                            \
+        (result)->tv_usec += 1000000;                  \
+      }                                                \
+    } while (0)
+  #endif
+ #ifndef timeradd
+ # define timeradd(a, b, result)			      \
+   do {							      \
+    (result)->tv_sec = (a)->tv_sec + (b)->tv_sec;	      \
+    (result)->tv_usec = (a)->tv_usec + (b)->tv_usec;	      \
+    if ((result)->tv_usec >= 1000000)			      \
+      {							      \
+	++(result)->tv_sec;				      \
+	(result)->tv_usec -= 1000000;			      \
+      }							      \
+   } while (0)
+ #endif
+#endif
+
+
 #ifdef HAVE_ADL
  #include "ADL_SDK/adl_sdk.h"
 #endif
