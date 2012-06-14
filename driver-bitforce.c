4fd53c9eb15cb603ba8837b20ab9383948582201
Paul Sheppard <shepsoft@gmail.com>
2012-06-14 00:04:37 -0700

    thread init fix

--- driver-bitforce.c
+++ driver-bitforce.c
@@ -372,8 +372,9 @@ static bool bitforce_get_stats(struct cgpu_info *bitforce)
 
 static bool bitforce_thread_init(struct thr_info *thr)
 {
+	struct cgpu_info *bitforce = thr->cgpu;
 	unsigned int wait;
-	thr;
+
 	/* Pause each new thread a random time between 0-100ms 
 	so the devices aren't making calls all at the same time. */
 	wait = (rand() * MAX_START_DELAY_US)/RAND_MAX;
