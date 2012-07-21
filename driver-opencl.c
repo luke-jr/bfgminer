ffe1318f7080db83c6e53d3560ee876fe1f57409
Con Kolivas <kernel@kolivas.org>
2012-07-21 16:05:50 +1000

    Ignore negative intensities for scrypt.

--- driver-opencl.c
+++ driver-opencl.c
@@ -1020,7 +1020,12 @@ static void set_threads_hashes(unsigned int vectors, unsigned int *threads,
 			       int64_t *hashes, size_t *globalThreads,
 			       unsigned int minthreads, int intensity)
 {
-	*threads = 1 << ((opt_scrypt ? 0 : 15) + intensity);
+	if (opt_scrypt) {
+		if (intensity < 0)
+			intensity = 0;
+		*threads = 1 << intensity;
+	} else
+		*threads = 1 << (15 + intensity);
 	if (*threads < minthreads)
 		*threads = minthreads;
 	*globalThreads = *threads;
