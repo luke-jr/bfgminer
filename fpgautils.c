18eaf31487faa168afd3625e3512a0a44f876d12
Luke Dashjr <luke-jr+git@utopios.org>
2012-08-18 18:51:45 +0000

    Revert "0 is a valid return value for read so only break out if read returns -1."
    This reverts commit 035ccdf1586a3cb2936c7973b6433cacca87d8cf.

--- fpgautils.c
+++ fpgautils.c
@@ -256,7 +256,7 @@ _serial_read(int fd, char *buf, size_t bufsiz, char *eol)
 	ssize_t len, tlen = 0;
 	while (bufsiz) {
 		len = read(fd, buf, eol ? 1 : bufsiz);
-		if (unlikely(len == -1))
+		if (len < 1)
 			break;
 		tlen += len;
 		if (eol && *eol == buf[0])
