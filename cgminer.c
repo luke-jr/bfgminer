78b2a47c0c8c1b7238434b20e18d9209dea3c5f0
Luke Dashjr <luke-jr+git@utopios.org>
2012-08-20 06:54:51 +0000

    Even if we want to submit stale shares, give up if they've failed and we have more submissions waiting on threads

--- cgminer.c
+++ cgminer.c
@@ -2284,8 +2284,12 @@ next_submit:
 			kill_work();
 			break;
 		}
-		else if (unlikely(work->stale && opt_retries < 0)) {
-			if (staleexpire <= time(NULL)) {
+		else if (work->stale) {
+			if (unlikely(!list_empty(&submit_waiting))) {
+				applog(LOG_WARNING, "Stale share failed to submit while queued submissions are waiting, discarding");
+				submit_discard_share(work);
+				break;
+			} else if (unlikely(opt_retries < 0 && staleexpire <= time(NULL))) {
 				applog(LOG_NOTICE, "Stale share failed to submit for 5 minutes, discarding");
 				submit_discard_share(work);
 				break;
