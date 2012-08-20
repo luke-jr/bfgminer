51d485e236586661771f0469ccf1598e3f80ae70
Con Kolivas <kernel@kolivas.org>
2012-08-20 12:31:43 +1000

    No need for extra variable in hash_push.

--- cgminer.c
+++ cgminer.c
@@ -2882,12 +2882,7 @@ static bool work_rollable(struct work *work)
 
 static bool hash_push(struct work *work)
 {
-	bool rc = true, dec = false;
-
-	if (work->queued) {
-		work->queued = false;
-		dec = true;
-	}
+	bool rc = true;
 
 	mutex_lock(stgd_lock);
 	if (work_rollable(work))
@@ -2900,8 +2895,10 @@ static bool hash_push(struct work *work)
 	pthread_cond_signal(&getq->cond);
 	mutex_unlock(stgd_lock);
 
-	if (dec)
+	if (work->queued) {
+		work->queued = false;
 		dec_queued();
+	}
 
 	return rc;
 }
