be06cf7083c26e5e332457f12213b1d4f0118aa1
Con Kolivas <kernel@kolivas.org>
2012-08-23 12:44:42 +1000

    Give warning with sdk2.7 and phatk as well.

--- ocl.c
+++ ocl.c
@@ -411,9 +411,9 @@ _clState *initCl(unsigned int gpu, char *name, size_t nameSize)
 		if (clState->chosen_kernel == KL_PHATK &&
 		    (strstr(vbuff, "844.4") || strstr(vbuff, "851.4") ||
 		     strstr(vbuff, "831.4") || strstr(vbuff, "898.1") ||
-		     strstr(vbuff, "923.1"))) {
+		     strstr(vbuff, "923.1") || strstr(vbuff, "938.1"))) {
 			applog(LOG_WARNING, "WARNING: You have selected the phatk kernel.");
-			applog(LOG_WARNING, "You are running SDK 2.6 which performs poorly with this kernel.");
+			applog(LOG_WARNING, "You are running SDK 2.6+ which performs poorly with this kernel.");
 			applog(LOG_WARNING, "Downgrade your SDK and delete any .bin files before starting again.");
 			applog(LOG_WARNING, "Or allow cgminer to automatically choose a more suitable kernel.");
 		}
