538ada6a9026c5ba6c5f61e7c31d548a10179ed7
Luke Dashjr <luke-jr+git@utopios.org>
2012-09-30 11:01:41 +0000

    Bugfix: Include nonce in data buffer for debugging

--- miner.c
+++ miner.c
@@ -2100,8 +2100,8 @@ static bool submit_upstream_work(const struct work *work, CURL *curl, bool resub
 	char hashshow[64+1] = "";
 
 	if (work->tmpl) {
-		unsigned char data[76];
-		swap32yes(data, work->data, 76 / 4);
+		unsigned char data[80];
+		swap32yes(data, work->data, 80 / 4);
 		json_t *req = blkmk_submit_jansson(work->tmpl, data, work->dataid, *((uint32_t*)&work->data[76]));
 		s = json_dumps(req, 0);
 		sd = bin2hex(data, 80);
