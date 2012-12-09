e6efec46e968ec901f2a1a92b2a2bf53265097ac
Kano <root@mutsumi.paige>
2012-12-10 10:42:02 +1100

    usbutils.c uninitialised usbstat for non-primary mmqs

--- usbutils.c
+++ usbutils.c
@@ -1061,6 +1061,9 @@ static void newstats(struct cgpu_info *cgpu)
 void update_usb_stats(__maybe_unused struct cgpu_info *cgpu)
 {
 #if DO_USB_STATS
+	if (cgpu->usbstat < 1)
+		newstats(cgpu);
+
 	// we don't know the device_id until after add_cgpu()
 	usb_stats[cgpu->usbstat - 1].device_id = cgpu->device_id;
 #endif
