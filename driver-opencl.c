606d7ef041b9dc7150eb4641a65e6cb68bb59a0f
Matthew Underwood <sakkaku@gmail.com>
2012-12-16 20:29:58 -0600

    Attempt to replace externals with headers

--- driver-opencl.c
+++ driver-opencl.c
@@ -45,158 +45,12 @@
 
 
 #ifdef HAVE_OPENCL
-/* Platform API */
-CL_API_ENTRY cl_int CL_API_CALL
-(*clGetPlatformIDs)(cl_uint          /* num_entries */,
-                 cl_platform_id * /* platforms */,
-                 cl_uint *        /* num_platforms */) CL_API_SUFFIX__VERSION_1_0;
 
-CL_API_ENTRY cl_int CL_API_CALL
-(*clGetPlatformInfo)(cl_platform_id   /* platform */,
-                  cl_platform_info /* param_name */,
-                  size_t           /* param_value_size */,
-                  void *           /* param_value */,
-                  size_t *         /* param_value_size_ret */) CL_API_SUFFIX__VERSION_1_0;
-
-/* Device APIs */
-CL_API_ENTRY cl_int CL_API_CALL
-(*clGetDeviceIDs)(cl_platform_id   /* platform */,
-               cl_device_type   /* device_type */,
-               cl_uint          /* num_entries */,
-               cl_device_id *   /* devices */,
-               cl_uint *        /* num_devices */) CL_API_SUFFIX__VERSION_1_0;
-
-CL_API_ENTRY cl_int CL_API_CALL
-(*clGetDeviceInfo)(cl_device_id    /* device */,
-                cl_device_info  /* param_name */,
-                size_t          /* param_value_size */,
-                void *          /* param_value */,
-                size_t *        /* param_value_size_ret */) CL_API_SUFFIX__VERSION_1_0;
-
-/* Context APIs  */
-CL_API_ENTRY cl_context CL_API_CALL
-(*clCreateContextFromType)(const cl_context_properties * /* properties */,
-                        cl_device_type          /* device_type */,
-                        void (CL_CALLBACK *     /* pfn_notify*/ )(const char *, const void *, size_t, void *),
-                        void *                  /* user_data */,
-                        cl_int *                /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
-
-CL_API_ENTRY cl_int CL_API_CALL
-(*clReleaseContext)(cl_context /* context */) CL_API_SUFFIX__VERSION_1_0;
-
-/* Command Queue APIs */
-CL_API_ENTRY cl_command_queue CL_API_CALL
-(*clCreateCommandQueue)(cl_context                     /* context */,
-                     cl_device_id                   /* device */,
-                     cl_command_queue_properties    /* properties */,
-                     cl_int *                       /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
-
-CL_API_ENTRY cl_int CL_API_CALL
-(*clReleaseCommandQueue)(cl_command_queue /* command_queue */) CL_API_SUFFIX__VERSION_1_0;
-
-/* Memory Object APIs */
-CL_API_ENTRY cl_mem CL_API_CALL
-(*clCreateBuffer)(cl_context   /* context */,
-               cl_mem_flags /* flags */,
-               size_t       /* size */,
-               void *       /* host_ptr */,
-               cl_int *     /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
-
-/* Program Object APIs  */
-CL_API_ENTRY cl_program CL_API_CALL
-(*clCreateProgramWithSource)(cl_context        /* context */,
-                          cl_uint           /* count */,
-                          const char **     /* strings */,
-                          const size_t *    /* lengths */,
-                          cl_int *          /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
-
-CL_API_ENTRY cl_program CL_API_CALL
-(*clCreateProgramWithBinary)(cl_context                     /* context */,
-                          cl_uint                        /* num_devices */,
-                          const cl_device_id *           /* device_list */,
-                          const size_t *                 /* lengths */,
-                          const unsigned char **         /* binaries */,
-                          cl_int *                       /* binary_status */,
-                          cl_int *                       /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
-
-CL_API_ENTRY cl_int CL_API_CALL
-(*clReleaseProgram)(cl_program /* program */) CL_API_SUFFIX__VERSION_1_0;
-
-CL_API_ENTRY cl_int CL_API_CALL
-(*clBuildProgram)(cl_program           /* program */,
-               cl_uint              /* num_devices */,
-               const cl_device_id * /* device_list */,
-               const char *         /* options */,
-               void (CL_CALLBACK *  /* pfn_notify */)(cl_program /* program */, void * /* user_data */),
-               void *               /* user_data */) CL_API_SUFFIX__VERSION_1_0;
-
-CL_API_ENTRY cl_int CL_API_CALL
-(*clGetProgramInfo)(cl_program         /* program */,
-                 cl_program_info    /* param_name */,
-                 size_t             /* param_value_size */,
-                 void *             /* param_value */,
-                 size_t *           /* param_value_size_ret */) CL_API_SUFFIX__VERSION_1_0;
-
-CL_API_ENTRY cl_int CL_API_CALL
-(*clGetProgramBuildInfo)(cl_program            /* program */,
-                      cl_device_id          /* device */,
-                      cl_program_build_info /* param_name */,
-                      size_t                /* param_value_size */,
-                      void *                /* param_value */,
-                      size_t *              /* param_value_size_ret */) CL_API_SUFFIX__VERSION_1_0;
-
-/* Kernel Object APIs */
-CL_API_ENTRY cl_kernel CL_API_CALL
-(*clCreateKernel)(cl_program      /* program */,
-               const char *    /* kernel_name */,
-               cl_int *        /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
-
-CL_API_ENTRY cl_int CL_API_CALL
-(*clReleaseKernel)(cl_kernel   /* kernel */) CL_API_SUFFIX__VERSION_1_0;
-
-CL_API_ENTRY cl_int CL_API_CALL
-(*clSetKernelArg)(cl_kernel    /* kernel */,
-               cl_uint      /* arg_index */,
-               size_t       /* arg_size */,
-               const void * /* arg_value */) CL_API_SUFFIX__VERSION_1_0;
-
-/* Flush and Finish APIs */
-CL_API_ENTRY cl_int CL_API_CALL
-(*clFinish)(cl_command_queue /* command_queue */) CL_API_SUFFIX__VERSION_1_0;
-
-/* Enqueued Commands APIs */
-CL_API_ENTRY cl_int CL_API_CALL
-(*clEnqueueReadBuffer)(cl_command_queue    /* command_queue */,
-                    cl_mem              /* buffer */,
-                    cl_bool             /* blocking_read */,
-                    size_t              /* offset */,
-                    size_t              /* size */,
-                    void *              /* ptr */,
-                    cl_uint             /* num_events_in_wait_list */,
-                    const cl_event *    /* event_wait_list */,
-                    cl_event *          /* event */) CL_API_SUFFIX__VERSION_1_0;
-
-CL_API_ENTRY cl_int CL_API_CALL
-(*clEnqueueWriteBuffer)(cl_command_queue   /* command_queue */,
-                     cl_mem             /* buffer */,
-                     cl_bool            /* blocking_write */,
-                     size_t             /* offset */,
-                     size_t             /* size */,
-                     const void *       /* ptr */,
-                     cl_uint            /* num_events_in_wait_list */,
-                     const cl_event *   /* event_wait_list */,
-                     cl_event *         /* event */) CL_API_SUFFIX__VERSION_1_0;
-
-CL_API_ENTRY cl_int CL_API_CALL
-(*clEnqueueNDRangeKernel)(cl_command_queue /* command_queue */,
-                       cl_kernel        /* kernel */,
-                       cl_uint          /* work_dim */,
-                       const size_t *   /* global_work_offset */,
-                       const size_t *   /* global_work_size */,
-                       const size_t *   /* local_work_size */,
-                       cl_uint          /* num_events_in_wait_list */,
-                       const cl_event * /* event_wait_list */,
-                       cl_event *       /* event */) CL_API_SUFFIX__VERSION_1_0;
+#ifdef __APPLE__
+    #include <OpenCL/cl.h>
+#else
+    #include "CL/cl.h"
+#endif
 
 #ifdef WIN32
 #define dlsym (void*)GetProcAddress
@@ -223,7 +77,7 @@ load_opencl_symbols() {
 		applog(LOG_ERR, "Failed to load OpenCL library, no GPUs usable");
 		return false;
 	}
-	
+
 	LOAD_OCL_SYM(clGetPlatformIDs);
 	LOAD_OCL_SYM(clGetPlatformInfo);
 	LOAD_OCL_SYM(clGetDeviceIDs);
@@ -246,7 +100,7 @@ load_opencl_symbols() {
 	LOAD_OCL_SYM(clEnqueueReadBuffer);
 	LOAD_OCL_SYM(clEnqueueWriteBuffer);
 	LOAD_OCL_SYM(clEnqueueNDRangeKernel);
-	
+
 	return true;
 }
 #endif
@@ -1074,7 +928,7 @@ static cl_int queue_poclbm_kernel(_clState *clState, dev_blk_ctx *blk, cl_uint t
 	CL_SET_BLKARG(cty_b);
 	CL_SET_BLKARG(cty_c);
 
-	
+
 	CL_SET_BLKARG(cty_f);
 	CL_SET_BLKARG(cty_g);
 	CL_SET_BLKARG(cty_h);
