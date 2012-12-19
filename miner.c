ed3a95aa1a7a64821fa9b1eed284575139a7fef7
Con Kolivas <kernel@kolivas.org>
2012-12-19 20:53:11 +1100

    Stop all work from the current pool if it's a stratum pool once it is disconnected since it will be invalid upon reconnecting.

--- miner.c
+++ miner.c
@@ -5516,6 +5516,8 @@ static void *stratum_thread(void *userdata)
 			 * the memory if we don't discard their records. */
 			clear_stratum_shares(pool);
 			clear_pool_work(pool);
+			if (pool == current_pool())
+				restart_threads();
 
 			if (initiate_stratum(pool) && auth_stratum(pool))
 				continue;
