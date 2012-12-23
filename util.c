bd15d76c47d26b257aaf6174749b1dc652371ff4
Luke Dashjr <luke-jr+git@utopios.org>
2012-12-23 23:47:52 +0000

    Explicitly cast all_data.buf to char* for debug printing

--- util.c
+++ util.c
@@ -453,7 +453,7 @@ json_t *json_rpc_call(CURL *curl, const char *url,
 		applog(LOG_INFO, "JSON decode failed(%d): %s", err.line, err.text);
 
 		if (opt_protocol)
-			applog(LOG_DEBUG, "JSON protocol response:\n%s", all_data.buf);
+			applog(LOG_DEBUG, "JSON protocol response:\n%s", (char*)all_data.buf);
 
 		goto err_out;
 	}
