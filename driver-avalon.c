b98e59a29f9add6509bba13e7916f6034129ae67
Xiangfu <xiangfu@openmobilefree.net>
2013-01-26 11:27:56 +0800

    change teh Copyright

--- driver-avalon.c
+++ driver-avalon.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Xiangfu
+ * Copyright 2013 Avalon project
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
--- driver-avalon.h
+++ driver-avalon.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Xiangfu <xiangfu@openmobilefree.com>
+ * Copyright 2013 Avalon project
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
