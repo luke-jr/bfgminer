a419763c07d626de2702772c38f9af3f355443ff
Luke Dashjr <luke-jr+git@utopios.org>
2013-02-04 14:15:42 +0000

    Bugfix: Sanity check for bits exponent in real_block_target
    Target is set to the highest possible in this case, so all shares will show as blocks

--- util.c
+++ util.c
@@ -586,6 +586,13 @@ void real_block_target(unsigned char *target, const unsigned char *data)
 {
 	uint8_t targetshift;
 
+	if (unlikely(data[72] < 3 || data[72] > 0x20))
+	{
+		// Invalid (out of bounds) target
+		memset(target, 0xff, 32);
+		return;
+	}
+
 	targetshift = data[72] - 3;
 	memset(target, 0, targetshift);
 	target[targetshift++] = data[75];
