571aa10a8d5f23c48d1459885f9e88b64a4869ac
Xiangfu <xiangfu@openmobilefree.net>
2013-02-16 22:54:26 +0800

    avalon.c: fix the copyright

--- driver-avalon.c
+++ driver-avalon.c
@@ -1,5 +1,7 @@
 /*
- * Copyright 2013 Avalon project
+ * Copyright 2012-2013 Xiangfu <xiangfu@openmobilefree.com>
+ * Copyright 2012 Luke Dashjr
+ * Copyright 2012 Andrew Smith
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
