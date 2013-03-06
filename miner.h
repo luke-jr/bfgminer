419083a7d28d6c43a45e22a2622a2ed0cdc5763d
Kano <root@mutsumi.paige>
2013-03-06 21:19:02 +1100

    API use control_lock when switching pools

--- miner.h
+++ miner.h
@@ -754,6 +754,7 @@ extern int opt_expiry;
 extern pthread_mutex_t cgusb_lock;
 #endif
 
+extern pthread_mutex_t control_lock;
 extern pthread_mutex_t hash_lock;
 extern pthread_mutex_t console_lock;
 extern pthread_mutex_t ch_lock;
