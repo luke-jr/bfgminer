daf62faa222d6c2c03043b1516957472dd740bbf
Con Kolivas <kernel@kolivas.org>
2013-03-07 15:10:38 +1100

    Clear the stratum socket whenever we are closing it since the buffer is going to be reused.
    Conflicts:
    	util.c

--- util.c
+++ util.c
@@ -1953,6 +1953,7 @@ out:
 
 void suspend_stratum(struct pool *pool)
 {
+	clear_sock(pool);
 	applog(LOG_INFO, "Closing socket for stratum pool %d", pool->pool_no);
 	mutex_lock(&pool->stratum_lock);
 	pool->stratum_active = false;
