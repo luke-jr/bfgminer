669fc3887aaf95a079131898960474383c1e04ec
Con Kolivas <kernel@kolivas.org>
2013-04-04 11:35:36 +1100

    Reinit avalon device in case of FPGA controller mess up.

--- driver-avalon.c
+++ driver-avalon.c
@@ -918,6 +918,8 @@ static int64_t avalon_scanhash(struct thr_info *thr, struct work **work,
 		applog(LOG_ERR,
 		       "AVA%i: FPGA controller mess up", avalon->device_id);
 		dev_error(avalon, REASON_DEV_COMMS_ERROR);
+		do_avalon_close(thr);
+		avalon_init(avalon);
 		return 0;
 	}
 
