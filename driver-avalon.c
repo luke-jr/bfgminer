f0806c682849a2d310081a41772022b41d8d52da
Con Kolivas <kernel@kolivas.org>
2013-04-05 18:34:05 +1100

    Actually put the work in the avalon queue.

--- driver-avalon.c
+++ driver-avalon.c
@@ -768,7 +768,7 @@ static bool avalon_fill(struct cgpu_info *avalon)
 
 	if (unlikely(!work))
 		return false;
-	avalon->queued++;
+	avalon->works[avalon->queued++] = work;
 	if (avalon->queued == avalon_info[avalon->device_id]->miner_count)
 		return true;
 	return false;
