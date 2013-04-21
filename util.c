3cc42231f18b5a24daf9afcd750733a84730eab2
Con Kolivas <kernel@kolivas.org>
2013-04-22 09:53:34 +1000

    Fulltest is true if value is <= target.

--- util.c
+++ util.c
@@ -660,7 +660,7 @@ bool fulltest(const unsigned char *hash, const unsigned char *target)
 		applog(LOG_DEBUG, " Proof: %s\nTarget: %s\nTrgVal? %s",
 			hash_str,
 			target_str,
-			rc ? "YES (hash < target)" :
+			rc ? "YES (hash <= target)" :
 			     "no (false positive; hash > target)");
 
 		free(hash_str);
