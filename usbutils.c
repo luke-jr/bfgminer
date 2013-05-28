adb2ea672d49a6578617d3ae60a4ca41c319b0aa
Con Kolivas <kernel@kolivas.org>
2013-05-28 20:50:53 +1000

    libusb_control_transfer are meant to be endian specific, but host endianness so no conversion is needed.

--- usbutils.c
+++ usbutils.c
@@ -2363,8 +2363,7 @@ int _usb_transfer(struct cgpu_info *cgpu, uint8_t request_type, uint8_t bRequest
 
 	STATS_TIMEVAL(&tv_start);
 	err = libusb_control_transfer(usbdev->handle, request_type,
-		bRequest, htole16(wValue), htole16(wIndex),
-		(unsigned char *)buf, (uint16_t)siz,
+		bRequest, wValue, wIndex, (unsigned char *)buf, (uint16_t)siz,
 		timeout == DEVTIMEOUT ? usbdev->found->timeout : timeout);
 	STATS_TIMEVAL(&tv_finish);
 	USB_STATS(cgpu, &tv_start, &tv_finish, err, MODE_CTRL_WRITE, cmd, SEQ0);
