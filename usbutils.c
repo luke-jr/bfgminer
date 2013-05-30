eec1a86af0129a1f24fadcc42182b74dfd0122f5
Con Kolivas <kernel@kolivas.org>
2013-05-30 22:53:07 +1000

    Update copyrights of modified files.

--- usbutils.c
+++ usbutils.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2012-2013 Andrew Smith
+ * Copyright 2013 Con Kolivas <kernel@kolivas.org>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
