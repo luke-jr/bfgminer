e22ef79ad583c4d52b698cf94eb707825d3e9821
Luke Dashjr <luke-jr+git@utopios.org>
2013-06-01 15:15:40 +0000

    ccan: Add missing copyright comment headers

--- ccan/opt/test/utils.c
+++ ccan/opt/test/utils.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2011 Rusty Russell
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.  See LICENSE for more details.
+ */
+
 #include "config.h"
 #include <ccan/tap/tap.h>
 #include <stdarg.h>
