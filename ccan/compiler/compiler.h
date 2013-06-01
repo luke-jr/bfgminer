d070ef943f7cb51b71916a52a5dba5a339af0e01
Luke Dashjr <luke-jr+git@utopios.org>
2013-06-01 15:15:40 +0000

    ccan: Add missing copyright comment headers

--- ccan/compiler/compiler.h
+++ ccan/compiler/compiler.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2011 Rusty Russell
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.  See LICENSE for more details.
+ */
+
 #ifndef CCAN_COMPILER_H
 #define CCAN_COMPILER_H
 #include "config.h"
