6789c96fd44c7ffd9c40e11838a00bc76040f4f6
Con Kolivas <kernel@kolivas.org>
2013-06-15 01:37:00 +1000

    Off by one error in device count for display.

--- cgminer.c
+++ cgminer.c
@@ -2100,7 +2100,7 @@ static void curses_print_devstatus(struct cgpu_info *cgpu, int count)
 	if (devcursor + count > LINES - 2)
 		return;
 
-	if (count > most_devices)
+	if (count >= most_devices)
 		return;
 
 	if (cgpu->dev_start_tv.tv_sec == 0)
