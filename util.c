83287b2e1220738c49a684d0c572fd02ea1fe612
ckolivas <kernel@kolivas.org>
2013-07-19 19:32:57 +1000

    Check for negative wait time in socket_full.

--- util.c
+++ util.c
@@ -1073,6 +1073,8 @@ static bool socket_full(struct pool *pool, int wait)
 	struct timeval timeout;
 	fd_set rd;
 
+	if (unlikely(wait < 0))
+		wait = 0;
 	FD_ZERO(&rd);
 	FD_SET(sock, &rd);
 	timeout.tv_usec = 0;
