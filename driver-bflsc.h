9a21a47334b77866aa0b2c617c8bc54eb52d0116
Con Kolivas <kernel@kolivas.org>
2013-08-09 15:17:12 +1000

    Update copyright notice.

--- driver-bflsc.h
+++ driver-bflsc.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2013 Con Kolivas <kernel@kolivas.org>
+ * Copyright 2013 Andrew Smith
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
