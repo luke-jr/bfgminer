0414bf0960ab7e5f9d7efbc51714e32d41b22bf9
Con Kolivas <kernel@kolivas.org>
2013-08-19 21:36:00 +1000

    Quotient/remainder error in ms division.

--- util.c
+++ util.c
@@ -915,7 +915,7 @@ void ms_to_timespec(struct timespec *spec, int64_t ms)
 	lldiv_t tvdiv = lldiv(ms, 1000);
 
 	spec->tv_sec = tvdiv.quot;
-	spec->tv_nsec = tvdiv.quot * 1000000;
+	spec->tv_nsec = tvdiv.rem * 1000000;
 }
 
 void timeraddspec(struct timespec *a, const struct timespec *b)
