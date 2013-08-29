99dc528c21311e952093b0a7d6e1695cffe46e63
ckolivas <kernel@kolivas.org>
2013-08-29 12:07:26 +1000

    Remove cached files from libusb autoconf on running autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,9 @@
 bs_dir="$(dirname $(readlink -f $0))"
 rm -rf "${bs_dir}"/autom4te.cache
 rm -f "${bs_dir}"/aclocal.m4 "${bs_dir}"/ltmain.sh
+libusb_dir="${bs_dir}"/libusb-1.0/
+rm -rf "${libusb_dir}"/autom4te.cache
+rm -rf "${libusb_dir}"/aclocal.m4 "${libusb_dir}"/ltmain.sh
 
 echo 'Running autoreconf -if...'
 aclocal --force -I m4
