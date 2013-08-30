748eda1faa759f30464df916e3adb080928cea1c
Con Kolivas <kernel@kolivas.org>
2013-08-30 14:47:53 +1000

    Make autoconf always build for libusb.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-make clean
-make maintainer-clean
-
 bs_dir="$(dirname $(readlink -f $0))"
 rm -rf "${bs_dir}"/autom4te.cache
 rm -f "${bs_dir}"/aclocal.m4 "${bs_dir}"/ltmain.sh
@@ -16,6 +13,8 @@ libtoolize --install --copy --force
 autoheader --force
 automake --add-missing --copy --force-missing
 autoconf --force
+
+autoreconf -fi "${libusb_dir}"
 
 if test -z "$NOCONFIGURE" ; then
 	echo 'Configuring...'
