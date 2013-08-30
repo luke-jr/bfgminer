a99f15bc1884d8692d363b0b6bb6f80fa3844850
Con Kolivas <kernel@kolivas.org>
2013-08-30 14:38:12 +1000

    More autoconf buggery.

--- autogen.sh
+++ autogen.sh
@@ -6,18 +6,16 @@ bs_dir="$(dirname $(readlink -f $0))"
 rm -rf "${bs_dir}"/autom4te.cache
 rm -f "${bs_dir}"/aclocal.m4 "${bs_dir}"/ltmain.sh
 
-echo 'Running autoreconf -if...'
-aclocal --force -I m4
-libtoolize --install --copy --force
-autoconf --force
-autoheader --force
-automake --add-missing --copy --force-missing
-
 libusb_dir="${bs_dir}"/compat/libusb-1.0/
 rm -rf "${libusb_dir}"/autom4te.cache
 rm -rf "${libusb_dir}"/aclocal.m4 "${libusb_dir}"/ltmain.sh
 
-autoreconf -fi "${libusb_dir}"
+echo 'Running autoreconf -if...'
+aclocal --force -I m4
+libtoolize --install --copy --force
+autoheader --force
+automake --add-missing --copy --force-missing
+autoconf --force
 
 if test -z "$NOCONFIGURE" ; then
 	echo 'Configuring...'
