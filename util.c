ddbd3ab78e81b14070c26ba662eca38dafc6c16d
Con Kolivas <kernel@kolivas.org>
2013-09-07 09:14:12 +1000

    Cope with trailing slashes in stratum urls.

--- util.c
+++ util.c
@@ -1107,9 +1107,14 @@ bool extract_sockaddr(char *url, char **sockaddr_url, char **sockaddr_port)
 
 	sprintf(url_address, "%.*s", url_len, url_begin);
 
-	if (port_len)
+	if (port_len) {
+		char *slash;
+
 		snprintf(port, 6, "%.*s", port_len, port_start);
-	else
+		slash = strchr(port, '/');
+		if (slash)
+			*slash = '\0';
+	} else
 		strcpy(port, "80");
 
 	*sockaddr_port = strdup(port);
