f317e829ab34c8f489fb8a66ceefdcfd353f8c6e
Con Kolivas <kernel@kolivas.org>
2013-09-19 09:41:25 +1000

    Create basic skeleton and copyright notices for hashfast driver.

---
+++ driver-hashfast.c
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2013 Con Kolivas <kernel@kolivas.org>
+ * Copyright 2013 Hashfast Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.  See COPYING for more details.
+ */
+
+#include "config.h"
---
+++ driver-hashfast.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2013 Con Kolivas <kernel@kolivas.org>
+ * Copyright 2013 Hashfast
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.  See COPYING for more details.
+ */
+
+#ifndef HASHFAST_H
+#define HASHFAST_H
+
+#ifdef USE_HASHFAST
+#endif /* USE_HASHFAST */
+#endif	/* HASHFAST_H */
