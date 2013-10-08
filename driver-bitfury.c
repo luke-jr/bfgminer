d81c6c3d826e4963cc31623b5d9e94a609fb4f91
Luke Dashjr <luke-jr+git@utopios.org>
2013-10-08 03:41:56 +0000

    bitfury: Include chip fasync in RPC status

--- driver-bitfury.c
+++ driver-bitfury.c
@@ -662,11 +662,12 @@ int64_t bitfury_job_process_results(struct thr_info *thr, struct work *work, boo
 
 struct api_data *bitfury_api_device_status(const struct cgpu_info * const cgpu)
 {
-	const struct bitfury_device * const bitfury = cgpu->device_data;
+	struct bitfury_device * const bitfury = cgpu->device_data;
 	struct api_data *root = NULL;
 	int clock_bits = bitfury->osc6_bits;
 	
 	root = api_add_int(root, "Clock Bits", &clock_bits, true);
+	root = api_add_uint(root, "fasync", &bitfury->fasync, false);
 	
 	return root;
 }
