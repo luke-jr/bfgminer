84382e10210d84c95b94c21e18321d9311354cd8
Con Kolivas <kernel@kolivas.org>
2013-10-16 23:17:55 +1100

    Work around older libtoolize that fails without top ltmain.sh not being present during autogen

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 bs_dir="$(dirname $(readlink -f $0))"
-rm -rf "${bs_dir}"/autom4te.cache "${bs_dir}"/aclocal.m4 "${bs_dir}"/ltmain.sh
 
+#Some versions of libtoolize don't like there being no ltmain.sh file already
+touch "${bs_dir}"/ltmain.sh
 autoreconf -fi "${bs_dir}"
 
 if test -z "$NOCONFIGURE" ; then
