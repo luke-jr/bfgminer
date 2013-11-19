7c3890c3bd3414fd1b143d487980abb6d05c4d73
ouɐɹɔs ʞɹɐɯ <mscrano@ieee.org>
2013-11-18 23:14:31 -0500

    line 2913 added urlencode
    There was a cross site scripting vulnerability due to insufficient input sanitation on the $pg parameter.  This patch fixes that issue.

--- miner.php
+++ miner.php
@@ -2941,7 +2941,7 @@ function display()
 
  if ($allowcustompages === true)
  {
-	$pg = trim(getparam('pg', true));
+	$pg = urlencode(trim(getparam('pg', true)));
 	if ($pagesonly === true)
 	{
 		if ($pg !== null && $pg !== '')
