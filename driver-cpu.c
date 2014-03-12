bf8bb53d2844fc10e582bde29eb3897b10a1f910
Kiyoaki Matsugae <matsugae.kiyoaki@gmail.com>
2014-03-12 12:05:18 +0900

    fix sched_setaffinity parameter : sizeof(cpu_set_t)

--- driver-cpu.c
+++ driver-cpu.c
@@ -64,7 +64,7 @@ static inline void affine_to_cpu(int id, int cpu)
 
 	CPU_ZERO(&set);
 	CPU_SET(cpu, &set);
-	sched_setaffinity(0, sizeof(&set), &set);
+	sched_setaffinity(0, sizeof(set), &set);
 	applog(LOG_INFO, "Binding cpu mining thread %d to cpu %d", id, cpu);
 }
 #else
