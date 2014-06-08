9e2b8f8b32dc5e8d94433a8e8c08edbc5c14a368
Luke Dashjr <luke-jr+git@utopios.org>
2014-06-08 21:45:21 +0000

    Bugfix: DevAPI: Make for_each_logical_proc work correctly

--- deviceapi.h
+++ deviceapi.h
@@ -126,6 +126,6 @@ extern void close_device_fd(struct thr_info *);
 #define for_each_managed_proc(procvar, dev)  \
 	for (struct cgpu_info *procvar = dev; procvar; procvar = procvar->next_proc)
 #define for_each_logical_proc(procvar, dev)  \
-	for (struct cgpu_info *procvar = dev; procvar->proc_id < dev->procs; procvar = procvar->next_proc)
+	for (struct cgpu_info *procvar = dev; procvar && procvar->device == (dev); procvar = procvar->next_proc)
 
 #endif
