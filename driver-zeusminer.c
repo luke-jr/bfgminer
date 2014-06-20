859de973282d1e5a2a773d44545812fb0a7fbf13
Nate Woolls <nwoolls@gmail.com>
2014-06-20 16:13:35 -0400

    zeusminer: refactoring: consistent prefixing

--- driver-zeusminer.c
+++ driver-zeusminer.c
@@ -16,15 +16,15 @@
 #include "driver-icarus.h"
 #include "lowl-vcom.h"
 
-#define ZEUSMINER_IO_SPEED		115200
+#define ZEUSMINER_IO_SPEED			115200
 
-#define ZEUS_CHIP_GEN1_CORES	8
-#define ZEUS_CHIP_CORES			ZEUS_CHIP_GEN1_CORES
-#define ZEUS_CHIPS_COUNT_MAX	1
-#define ZEUS_CHIPS_COUNT		6
-#define ZEUS_DEFAULT_CLOCK		328
-#define ZEUSMINER_MIN_CLOCK		200
-#define ZEUSMINER_MAX_CLOCK		383
+#define ZEUSMINER_CHIP_GEN1_CORES	8
+#define ZEUSMINER_CHIP_CORES		ZEUSMINER_CHIP_GEN1_CORES
+#define ZEUSMINER_CHIPS_COUNT_MAX	1
+#define ZEUSMINER_CHIPS_COUNT		6
+#define ZEUSMINER_DEFAULT_CLOCK		328
+#define ZEUSMINER_MIN_CLOCK			200
+#define ZEUSMINER_MAX_CLOCK			383
 
 
 BFG_REGISTER_DRIVER(zeusminer_drv)
@@ -79,8 +79,8 @@ bool zeusminer_detect_one(const char *devpath)
 		.fpga_count = 1,
 		.probe_read_count = 5,
 		.golden_nonce = (char*)scrypt_golden_nonce,
-		.chips = ZEUS_CHIPS_COUNT,
-		.freq = ZEUS_DEFAULT_CLOCK,
+		.chips = ZEUSMINER_CHIPS_COUNT,
+		.freq = ZEUSMINER_DEFAULT_CLOCK,
 	};
 
 	//pick up any user-defined settings passed in via --set
@@ -91,7 +91,7 @@ bool zeusminer_detect_one(const char *devpath)
 	//the nonce range is split into 2^(10+3) parts
 	info->work_division = 8192;
 
-	info->fpga_count = info->chips * ZEUS_CHIP_CORES;
+	info->fpga_count = info->chips * ZEUSMINER_CHIP_CORES;
 
 	//send the requested Chip Speed with the detect golden OB
 	//we use the time this request takes in order to calc hashes
@@ -111,7 +111,7 @@ bool zeusminer_detect_one(const char *devpath)
 
 	double duration_sec;
 	const double hash_count = (double)0xd26;
-	uint64_t default_hashes_per_core = (((info->freq * 2) / 3) * 1024) / ZEUS_CHIP_CORES;
+	uint64_t default_hashes_per_core = (((info->freq * 2) / 3) * 1024) / ZEUSMINER_CHIP_CORES;
 
 	if (info->ignore_golden_nonce)
 		duration_sec = hash_count / default_hashes_per_core;
@@ -119,16 +119,16 @@ bool zeusminer_detect_one(const char *devpath)
 		duration_sec = ((double)(info->golden_tv.tv_sec) + ((double)(info->golden_tv.tv_usec)) / ((double)1000000));
 
 	//determines how the hash rate is calculated when no nonce is returned
-	info->Hs = (double)(duration_sec / hash_count / info->chips / ZEUS_CHIP_CORES);
+	info->Hs = (double)(duration_sec / hash_count / info->chips / ZEUSMINER_CHIP_CORES);
 
 	//set the read_count (how long to wait for a result) based on chips, cores, and time to find a nonce
-	int chips_count_max = ZEUS_CHIPS_COUNT_MAX;
+	int chips_count_max = ZEUSMINER_CHIPS_COUNT_MAX;
 	if (info->chips > chips_count_max)
 		chips_count_max = upper_power_of_two(info->chips);
 	//golden_speed_per_core is the number of hashes / second / core
 	uint64_t golden_speed_per_core = (uint64_t)(hash_count / duration_sec);
 	//don't combine the following two lines - overflows leaving info->read_count at 0
-	info->read_count = (uint32_t)((4294967296 * 10) / (ZEUS_CHIP_CORES * chips_count_max * golden_speed_per_core * 2));
+	info->read_count = (uint32_t)((4294967296 * 10) / (ZEUSMINER_CHIP_CORES * chips_count_max * golden_speed_per_core * 2));
 	info->read_count = info->read_count * 3/4;
 
 	return true;
