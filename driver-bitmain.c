09f6d0f7f75f8e443fea03a29516660d8bb09a06
Luke Dashjr <luke-jr+git@utopios.org>
2015-09-26 17:44:11 +0000

    bitmain: Simplify num2bit

--- driver-bitmain.c
+++ driver-bitmain.c
@@ -284,41 +284,7 @@ static uint16_t CRC16(const uint8_t* p_data, uint16_t w_len)
 }
 
 static uint32_t num2bit(int num) {
-	switch(num) {
-	case 0:  return 0x80000000;
-	case 1:  return 0x40000000;
-	case 2:  return 0x20000000;
-	case 3:  return 0x10000000;
-	case 4:  return 0x08000000;
-	case 5:  return 0x04000000;
-	case 6:  return 0x02000000;
-	case 7:  return 0x01000000;
-	case 8:  return 0x00800000;
-	case 9:  return 0x00400000;
-	case 10: return 0x00200000;
-	case 11: return 0x00100000;
-	case 12: return 0x00080000;
-	case 13: return 0x00040000;
-	case 14: return 0x00020000;
-	case 15: return 0x00010000;
-	case 16: return 0x00008000;
-	case 17: return 0x00004000;
-	case 18: return 0x00002000;
-	case 19: return 0x00001000;
-	case 20: return 0x00000800;
-	case 21: return 0x00000400;
-	case 22: return 0x00000200;
-	case 23: return 0x00000100;
-	case 24: return 0x00000080;
-	case 25: return 0x00000040;
-	case 26: return 0x00000020;
-	case 27: return 0x00000010;
-	case 28: return 0x00000008;
-	case 29: return 0x00000004;
-	case 30: return 0x00000002;
-	case 31: return 0x00000001;
-	default: return 0x00000000;
-	}
+	return 1L << (31 - num);
 }
 
 static int bitmain_set_txconfig(struct bitmain_txconfig_token *bm,
